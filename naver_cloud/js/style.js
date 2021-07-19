e8d86d6714eb773730c6c98b21af2993a49f4e27
yesolu <kysol1203@naver.com>
2021-07-20 02:12:00 +0900

    네이버 클라우드 클론코딩_section 수정

---
+++ naver_cloud/js/style.js
@@ -0,0 +1,13 @@
+$(document).ready(function(){
+    $('.gnb_link').on('click',function(){
+      $('.gnb_link').removeClass('on')
+      $(this).addClass('on')
+
+      var i = $(this).index()
+      
+  
+    })
+  
+  });
+
+  
