1de7757bd7af1c130530348ca2b1782bb66db236
yesolu <kysol1203@naver.com>
2021-07-22 23:33:53 +0900

    네이버 클라우드 클론코딩_2

--- naver_cloud/js/style.js
+++ naver_cloud/js/style.js
@@ -10,4 +10,39 @@ $(document).ready(function(){
   
   });
 
+  $(document).ready(function(){
+    $('.lnb_link a').on('click',function(){
+      $('.lnb_link a').removeClass('on')
+      $(this).addClass('on')
+
+      var i = $(this).index()
+      
+  
+    })
+  
+  });
+
+  $(document).ready(function(){
+    $('.lang').on('click',function(){
+      $('.lnb').toggleClass('on')
+      $('.lnb').slideToggle('slow')
+
+})
+
+
+  
+  });
+
+  $(window).scroll(function(){
+    //스크롤이 될때마다 위치를 담아라
+    var scrollTopPus = $(window).scrollTop();
+    if(scrollTopPus > 0){//스크롤을 아래로 내리는 순간 처리
+        $("body").addClass('scroll');
+    }
+    else{//스크롤이 완전 위에 닿았을때 처리
+        $('body').removeClass('scroll');
+    }
+
+
+});
   
